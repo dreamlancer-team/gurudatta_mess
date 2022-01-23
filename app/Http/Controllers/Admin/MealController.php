09161f518663a9858e02ef73066336f21e8de993
unknown <aniketkhote99@gmail.com>
2022-01-23 18:42:23 +0530

    admin scaffolding created

---
+++ app/Http/Controllers/Admin/MealController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Meal;
+use App\Http\Requests\Admin\StoreMealRequest;
+use App\Http\Requests\Admin\UpdateMealRequest;
+
+class MealController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Admin\StoreMealRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMealRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Admin\UpdateMealRequest  $request
+     * @param  \App\Models\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMealRequest $request, Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Meal $meal)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Admin/PackageController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Package;
+use App\Http\Requests\Admin\StorePackageRequest;
+use App\Http\Requests\Admin\UpdatePackageRequest;
+
+class PackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Admin\StorePackageRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePackageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Package $package)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Package $package)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Admin\UpdatePackageRequest  $request
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePackageRequest $request, Package $package)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Package $package)
+    {
+        //
+    }
+}
